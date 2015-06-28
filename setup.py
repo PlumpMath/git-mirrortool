3c7aee87a4d8ee2acb00e313acb00c35c5d30982
Ian Denhardt <ian@zenhack.net>
2015-06-28 15:10:16 -0400

    Add git-mirrortool script
    Doesn't do much yet, and fails because of a bug re: basic auth

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='git-mirrortool',
       version='0.1',
       packages=['git_mirrortool'],
-#      scripts=['bin/git-mirrortool'],
+      scripts=['bin/git-mirrortool'],
       install_requires=['requests'],
       author='Ian Denhardt',
       author_email='ian@zenhack.net',
