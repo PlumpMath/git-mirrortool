a71b5603e8f50a19b6bc264788c2b4f189bdd2c1
Ian Denhardt <ian@zenhack.net>
2015-06-28 15:00:16 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='git-mirrortool',
+      version='0.1',
+      packages=['git_mirrortool'],
+#      scripts=['bin/git-mirrortool'],
+      install_requires=['requests'],
+      author='Ian Denhardt',
+      author_email='ian@zenhack.net',
+      description='Tool for making mirroring git repos in multiple places easier',
+      license='MIT',
+      url='https://github.com/zenhack/git-mirrortool',
+      )
