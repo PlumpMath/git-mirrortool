87f5ff0e93bd3c27efb133b82601a2807b489fd9
Ian Denhardt <ian@zenhack.net>
2015-11-20 19:27:41 -0500

    github: General refactoring
    * Most stuff is in the client class now.
    * Pass in Accept: header
    * Display full http response on error
    * API endpoint is configurable (defaults to github.com).

--- git_mirrortool/github.py
+++ git_mirrortool/github.py
@@ -7,59 +7,79 @@ from git_mirrortool import errors
 from git_mirrortool.git import git
 
 
-API_ENDPOINT = 'https://api.github.com'
+DEFAULT_API_ENDPOINT = 'https://api.github.com'
 
 class Client(object):
 
-    def __init__(self, token):
+    def __init__(self, username, token=None, endpoint=DEFAULT_API_ENDPOINT):
+        self.endpoint = endpoint
+        self.username = username
+        self.default_headers = {
+            'Accept': 'application/vnd.github.v3+json',
+        }
+        if token is not None:
+            self._set_token(token)
+
+    def _set_token(self, token):
         self.token = token
+        self.default_headers['Authorization'] = 'token %s' % self.token
 
     def create_repo(self, name):
         response = requests.post(
-            API_ENDPOINT + '/user/repos',
-            headers={'Authorization': 'token %s' % self.token},
+            self.endpoint + '/user/repos',
+            headers=self.default_headers,
             data=json.dumps({
                 'name': name,
             })
         )
-
-def get_token(username, password):
-    response = requests.post(
-        API_ENDPOINT + '/authorizations',
-        auth=(username, password),
-        data=json.dumps({
-            'scopes': ['repo'],
-            'note': 'git-mirrortool',
-        })
-    )
-    if response.status_code < 200 or response.status_code >= 300:
-        raise errors.RequestFailed(response)
-    try:
-        result = response.json()
-        return result['hashed_token']
-    except ValueError:
-        raise errors.UnexpectedResponse(response, "Body is not JSON")
+        response.text
 
 
-def save_config(accountname, username, token):
-    account = 'mirrortool.account.%s' % accountname
-    git(['config', '--global', '%s.type' % account,  'github'])
-    git(['config', '--global', '%s.username' % account, username])
-    git(['config', '--global', '%s.token' % account, token])
+    def get_token(self, password):
+        response = requests.post(
+            self.endpoint + '/authorizations',
+            auth=(self.username, password),
+            headers=self.default_headers,
+            data=json.dumps({
+                'scopes': ['repo'],
+                'note': 'git-mirrortool',
+            })
+        )
+        if response.status_code < 200 or response.status_code >= 300:
+            raise errors.RequestFailed(response)
+        try:
+            result = response.json()
+            self._set_token(result['hashed_token'])
+        except ValueError:
+            raise errors.UnexpectedResponse(response, "Body is not JSON")
 
+    def save_token(self, accountname):
+        account = 'mirrortool.account.%s' % accountname
+        git(['config', '--global', '%s.type' % account,  'github'])
+        git(['config', '--global', '%s.username' % account, self.username])
+        git(['config', '--global', '%s.token' % account, self.token])
 
-def client(accountname):
-    token = git(['config', '--global', 'mirrortool.account.%s.token' %
-        accountname])
-    return Client(token)
+    @staticmethod
+    def from_config(accountname):
+        token = git(['config', '--global', 'mirrortool.account.%s.token' %
+                     accountname])
+        return Client(token)
+
 
 def prompt():
     sys.stdout.write('Github Username: ')
     username = raw_input()
     password = getpass()
-    token = get_token(username, password)
+    client = Client(username)
+    try:
+        client.get_token(password)
+    except errors.RequestFailed as e:
+        print("Request failed. Response:")
+        print(e.response.text)
+        return
     accountname = 'github/%s' % username
-    save_config(accountname, username, token)
+    client.save_token(accountname)
+
     sys.stdout.write('Enable by default? [Y/n]: ')
     enable = raw_input()
     if enable in ('Y', 'y', ''):
